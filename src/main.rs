da4be7bcd61a3ed4588a00dc3f8a1a76603a01fb
Malanca Daniel <malanca.daniel23@gmail.com>
2021-12-16 21:52:01 +0200

    new working version no lag

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
-use std::time::Duration;
-use crossterm::{event::{read, poll}, event};
-use crossterm::terminal;
-use std::vec::Vec;
-use std::io::stdout;
 use crossterm::queue;
+use crossterm::terminal;
+use crossterm::{event, event::read};
+use std::io::stdout;
 use std::io::Write;
-// use std::thread;
-// use std::sync::mpsc;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+use std::vec::Vec;
 
 const FILLED: char = '\u{2588}';
 const EMPTY: char = ' ';
-const DIRECTIONS: [(i32, i32); 8] = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)];
-const FRAMES_PER_UPDATE : u32 = 1;//: Duration = Duration::from_millis(200);
+const DIRECTIONS: [(i32, i32); 8] = [
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (-1, 0),
+    (1, 1),
+    (1, -1),
+    (-1, 1),
+    (-1, -1),
+];
+const FRAMES_PER_UPDATE: u32 = 1;
 const TIME_FRAME: Duration = Duration::from_millis(1000 / 60);
 
+enum Event {
+    Break(bool),
+    Pause(bool),
+    Click(i32, i32),
+}
+
 fn fill(filled: bool) -> char {
-    if filled { FILLED } else { EMPTY }
+    if filled {
+        FILLED
+    } else {
+        EMPTY
+    }
 }
 
 fn main() {
@@ -24,54 +43,73 @@ fn main() {
     let mut table = vec![vec![false; columns as usize]; rows as usize];
 
     crossterm::execute!(std::io::stdout(), crossterm::event::EnableMouseCapture);
-    // print_events();
 
     let mut stdout = stdout();
     let mut counter = 0;
 
-    loop {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        read_events(tx);
+    });
+    'main: loop {
+        loop {
+            match rx.try_recv() {
+                Ok(x) => match x {
+                    Event::Break(_) => {
+                        break 'main;
+                    }
+                    Event::Pause(_) => {
+                        paused = !paused;
+                    }
+                    Event::Click(x, y) => {
+                        table[x as usize][y as usize] = !table[x as usize][y as usize];
+                    }
+                },
+                Err(_) => {
+                    break;
+                }
+            }
+        }
+        let instant = std::time::Instant::now();
         counter += 1;
 
-        if !paused && (counter % FRAMES_PER_UPDATE == 0){
+        if !paused && (counter % FRAMES_PER_UPDATE == 0) {
             update_table(&mut table);
             counter = 0;
         }
 
         crossterm::execute!(stdout, crossterm::cursor::MoveTo(0, 0));
         for row in &table {
-            for &cell in row { 
+            for &cell in row {
                 queue!(stdout, crossterm::style::Print(fill(cell)));
             }
         }
-        
         stdout.flush().unwrap();
-        
-        if read_events(&mut table, &mut paused) {
-            break;
+        let time = instant.elapsed();
+        if time < TIME_FRAME {
+            thread::sleep(TIME_FRAME - instant.elapsed());
         }
     }
-        
 }
 
 fn update_table(table: &mut Vec<Vec<bool>>) {
     let mut place_holder = table.clone();
     for (x, row) in table.iter().enumerate() {
         for (y, _) in row.iter().enumerate() {
-            let mut live_cells : u32 = 0;
+            let mut live_cells: u32 = 0;
             for dir in DIRECTIONS {
-                 match table.get(((x as i32) + dir.0) as usize) {
-                     Some(x) => {
-                        match x.get(((y as i32) +dir.1) as usize) {
-                            Some(flag) => {
-                                if *flag {
-                                    live_cells += 1;
-                                }
-                            },
-                            None => ()
+                match table.get(((x as i32) + dir.0) as usize) {
+                    Some(x) => match x.get(((y as i32) + dir.1) as usize) {
+                        Some(flag) => {
+                            if *flag {
+                                live_cells += 1;
+                            }
                         }
-                     },
-                     None => ()
-                 }
+                        None => (),
+                    },
+                    None => (),
+                }
             }
 
             if (live_cells == 2 || live_cells == 3) && table[x][y] {
@@ -87,36 +125,31 @@ fn update_table(table: &mut Vec<Vec<bool>>) {
     *table = place_holder.clone();
 }
 
-fn read_events(table : &mut Vec<Vec<bool>>, paused : &mut bool) -> bool {
-    let instant = std::time::Instant::now();
-    while TIME_FRAME > instant.elapsed() {
-        if poll(TIME_FRAME - instant.elapsed()).unwrap() {
-            let ev = read().unwrap();
-            match ev {
-                event::Event::Mouse(x) => match x.kind {
-                    event::MouseEventKind::Down(_) => {
-                        table[x.row as usize][x.column as usize] = !table[x.row as usize][x.column as usize];
-                    },
-                    event::MouseEventKind::Drag(_) => {
-                        table[x.row as usize][x.column as usize] = !table[x.row as usize][x.column as usize];
-                        read_events(table, paused); 
-                    }
-                    _ => ()
-                },
-                event::Event::Key(x) => {
-                    if x.code == event::KeyCode::Char('c') && x.modifiers == event::KeyModifiers::CONTROL {
-                        return true;
-                    } else if x.code == event::KeyCode::Char('p') {
-                        *paused = !(*paused);
-                    };
-                },
-                _ => ()
+fn read_events(tx: mpsc::Sender<Event>) {
+    loop {
+        let ev = read().unwrap();
+        match ev {
+            event::Event::Mouse(x) => match x.kind {
+                event::MouseEventKind::Down(_) => {
+                    tx.send(Event::Click(x.row as i32, x.column as i32))
+                        .unwrap();
                 }
+                event::MouseEventKind::Drag(_) => {
+                    tx.send(Event::Click(x.row as i32, x.column as i32))
+                        .unwrap();
+                }
+                _ => (),
+            },
+            event::Event::Key(x) => {
+                if x.code == event::KeyCode::Char('c')
+                    && x.modifiers == event::KeyModifiers::CONTROL
+                {
+                    tx.send(Event::Break(true)).unwrap();
+                } else if x.code == event::KeyCode::Char('p') {
+                    tx.send(Event::Pause(true)).unwrap();
+                };
+            }
+            _ => (),
         }
     }
-    // let x = instant.elapsed();
-    // if x < TIME_FRAME {
-    //     std::thread::sleep(TIME_FRAME - x);
-    // } 
-    return false;
 }
